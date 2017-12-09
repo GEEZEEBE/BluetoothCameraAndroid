710518d306714ffa85a286b120775ce7fdbfa189
SanghunOh <otter.oh@gmail.com>
2017-12-09 19:27:46 +0900

    completed version up and test

--- app/src/main/java/com/example/bltcamera/modules/main/MainActivity.java
+++ app/src/main/java/com/example/bltcamera/modules/main/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.bltcamera.modules.main;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.example.bltcamera.R;
@@ -17,6 +23,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH);
+
+        if(permission != PackageManager.PERMISSION_GRANTED){
+            makeRequest();
+        }
         if (BluetoothAdapter.getDefaultAdapter() == null) {
             setAlertVisible();
         } else {
@@ -24,6 +35,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    private void makeRequest() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH}, 101);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 101:
+                if(grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Log.d("makeRequest","denied");
+
+                } else {
+                    Log.d("makeRequest","granted");
+                }
+                break;
+        }
+    }
     private void setAlertVisible() {
         View view = findViewById(R.id.activity_main_exit);
         view.setVisibility(View.VISIBLE);
